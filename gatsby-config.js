0d72f9ae7a23b72612c24e59ac49e915e48b923a
Mayyyyyyyy <8633009+mayyyyyyyy@user.noreply.gitee.com>
2021-02-28 16:00:09 +0800

    first edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,41 @@
+const config = require("./data/siteConfig");
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API',
+    title: config.siteTitle,
+    description: config.siteDescription,
+    author: config.authorName,
+    ...config
   },
+  pathPrefix: config.pathPrefix,
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gatsby + Node.js (TypeScript) API',
-        short_name: 'Gatsby + Node.js (TypeScript)',
-        start_url: '/',
-        icon: 'src/images/gatsby-icon.png',
+        name: config.siteTitle,
+        short_name: config.siteTitle,
+        start_url: config.pathPrefix,
+        background_color: config.background_color,
+        theme_color: config.theme_color,
+        display: config.display,
+        icon: config.icon,
       },
     },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          config.googleAnalyticsId // Google Analytics / GA
+        ],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
   ],
-};
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,133 @@
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import '../styles/index.css';
+import React from 'react'
+import styled from 'styled-components'
+import { Container, Row, Col } from 'react-awesome-styled-grid'
+import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from "react-icons/fa"
+import siteConfig from '../../data/siteConfig'
+import { withPrefix } from "gatsby"
+import loadable from '@loadable/component'
 
-function Index() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+import Hero from '../components/hero'
+import SEO from '../components/SEO'
+import Wrapper from '../components/wrapper'
+import About from '../components/about'
+import Skills from '../components/skills'
+import Timeline from '../components/timeline'
+import Repositories from '../components/repositories'
+
+const Layout = loadable(() => import('../components/layout'))
+
+const Separator = styled.hr`
+  margin-top: 24px;
+  margin-bottom: 16px;
+  background-color: ${({ theme }) => theme.colors.fontColor};
+  opacity: .2;
+`
+
+const Home = ({ className, location }) => {
+  // validate siteConfig settings
+  if (siteConfig.googleAnalyticsId === 'UA-000000000-1') {
+    console.error('WARNING: Please set a proper googleAnalyticsId. See https://analytics.google.com for details.');
+  }
+
+  const title = siteConfig.siteTitle
+  const { keywords } = siteConfig
   return (
-    <main>
-      <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
-      </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+    <Layout location={location}>
+      <SEO
+        title={title}
+        keywords={keywords}
+      />
+
+      <Hero
+        heroImg={siteConfig.siteCover}
+        title={title}
+      />
+
+      <Wrapper className={className} >
+        <Container className="page-content" fluid>
+          <Row>
+            <Col xs={4} className='avatar'>
+              <img
+                className='avatar__image'
+                src={withPrefix(siteConfig.authorAvatar)}
+                alt='user avatar'
+              />
+              <div className="social">
+                {siteConfig.social.github && <a className="social-link github" href={siteConfig.social.github}>
+                  <FaGithub className="social-icon" size="32" />
+                </a>}
+                {siteConfig.social.linkedin && <a className="social-link linkedin" href={siteConfig.social.linkedin}>
+                  <FaLinkedin className="social-icon" size="32" />
+                </a>}
+                {siteConfig.social.twitter && <a className="social-link twitter" href={siteConfig.social.twitter}>
+                  <FaTwitter className="social-icon" size="32" />
+                </a>}
+                {siteConfig.social.email && <a className="social-link email" href={`mailto:${siteConfig.social.email}`}>
+                  <FaEnvelope className="social-icon" size="32" />
+                </a>}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={4} sm={4}>
+              <About title='About' text={siteConfig.authorDescription}/>
+            </Col>
+            <Col xs={4} sm={4}>
+              <Skills title='Skills' skills={siteConfig.skills} />
+            </Col>
+          </Row>
+          <Separator />
+          <Timeline />
+          <Separator />
+          {siteConfig.githubUsername && <Repositories />}
+        </Container>
+      </Wrapper>
+    </Layout>
+  )
 }
 
-export default Index;
+export default styled(Home)`
+  .page-content {
+    max-width: 100%;
+    margin-bottom: 40px;
+  }
+
+  .avatar {
+    align-items: center;
+    margin-bottom: 24px;
+    flex-direction: column;
+  }
+
+  .avatar__image {
+    box-shadow: 3px 3px 15px 0px rgba(0,0,0,0.75);
+    max-width: 200px;
+    border-radius: 100px;
+    margin: 0 auto 24px;
+  }
+
+  .social {
+    margin-top: 12px;
+    margin-bottom: 12px;
+  }
+
+  .social-link {
+    padding: 8px;
+    color: #555;
+  }
+
+  a.social-link.twitter:hover {
+    color: #1da1f2;
+  }
+
+  a.social-link.github:hover {
+    color: #24292e;
+  }
+
+  a.social-link.linkedin:hover {
+    color: #0077B5;
+  }
+
+  a.social-link.email:hover {
+    color: #c23a2b;
+  }
+`
